97d3f12112f9d39e5f2ee6fba283ad822f3b2a87
asifulalamkn <asifulalamkn@gmail.com>
2026-05-04 00:47:45 +0600

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,28 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "**", 
+        hostname: "ui-avatars.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "covers.openlibrary.org",
+      },
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.shutterstock.com",
+      }
     ],
   },
 };
